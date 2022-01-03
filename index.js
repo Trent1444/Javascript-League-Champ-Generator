480a5617b9d17e6fc79b202938fcfe4e46aa2777
TrentDev <TrentLathim@gmail.com>
2022-01-02 23:44:32 -0800

    created manual adc image generation with changing index of array

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const 
+const adcImages = [
+  "images/adc/ashe.jpg",
+  "images/adc/jhin.jpg",
+  "images/adc/twitch.jpg",
+];
+function getRandomAdc() {
+  document.getElementById("championImage").src = adcImages[1];
+}
