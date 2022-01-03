62369371e26026b5b840a3801913f0e0804d9c55
TrentDev <TrentLathim@gmail.com>
2022-01-03 00:35:56 -0800

    created perfect adc random functionality

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const adcImages = [
   "images/adc/ashe.jpg",
   "images/adc/jhin.jpg",
   "images/adc/twitch.jpg",
+  "images/adc/draven.jpg",
+  "images/adc/ezreal.jpg",
+  "images/adc/jinx.jpg",
+  "images/adc/tristana.jpg",
 ];
+
 function getRandomAdc() {
-  document.getElementById("championImage").src = adcImages[1];
+  const randomAdc = adcImages[Math.floor(Math.random() * adcImages.length)];
+  document.getElementById("championImage").src = randomAdc;
 }
