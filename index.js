0b1035f9921d9f9734160fd2b6c7bbef303fad0c
TrentDev <TrentLathim@gmail.com>
2022-01-03 00:50:47 -0800

    added outline for other roles, coppied from adc logic

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+const topImages = [
+  "images/adc/ashe.jpg",
+  "images/adc/jhin.jpg",
+  "images/adc/twitch.jpg",
+  "images/adc/draven.jpg",
+  "images/adc/ezreal.jpg",
+  "images/adc/jinx.jpg",
+  "images/adc/tristana.jpg",
+];
+
+function getRandomTop() {
+  const randomTop = topImages[Math.floor(Math.random() * TopImages.length)];
+  document.getElementById("championImage").src = randomTop;
+}
+
+const jungleImages = [
+  "images/adc/ashe.jpg",
+  "images/adc/jhin.jpg",
+  "images/adc/twitch.jpg",
+  "images/adc/draven.jpg",
+  "images/adc/ezreal.jpg",
+  "images/adc/jinx.jpg",
+  "images/adc/tristana.jpg",
+];
+
+function getRandomJungle() {
+  const randomJungle =
+    jungleImages[Math.floor(Math.random() * jungleImages.length)];
+  document.getElementById("championImage").src = randomJungle;
+}
+
+const midImages = [
+  "images/adc/ashe.jpg",
+  "images/adc/jhin.jpg",
+  "images/adc/twitch.jpg",
+  "images/adc/draven.jpg",
+  "images/adc/ezreal.jpg",
+  "images/adc/jinx.jpg",
+  "images/adc/tristana.jpg",
+];
+
+function getRandomMid() {
+  const randomMid = midImages[Math.floor(Math.random() * midImages.length)];
+  document.getElementById("championImage").src = randomMid;
+}
+
 const adcImages = [
   "images/adc/ashe.jpg",
   "images/adc/jhin.jpg",
@@ -11,4 +57,20 @@ const adcImages = [
 function getRandomAdc() {
   const randomAdc = adcImages[Math.floor(Math.random() * adcImages.length)];
   document.getElementById("championImage").src = randomAdc;
+}
+
+const supportImages = [
+  "images/adc/ashe.jpg",
+  "images/adc/jhin.jpg",
+  "images/adc/twitch.jpg",
+  "images/adc/draven.jpg",
+  "images/adc/ezreal.jpg",
+  "images/adc/jinx.jpg",
+  "images/adc/tristana.jpg",
+];
+
+function getRandomAdc() {
+  const randomSupport =
+    supportImages[Math.floor(Math.random() * supportImages.length)];
+  document.getElementById("championImage").src = randomSupport;
 }
